6848fcb4c250714fedb60235b13c59571909adcd
racarlosdavid <2111724640101@ingenieria.usac.edu.gt>
2022-03-26 23:18:43 -0600

    feat: add endpoints of backend

--- backend-one-time-secret/routes/index.js
+++ backend-one-time-secret/routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-    res.status(200).send("Server TFive - Up and Running");
+    res.status(200).send("Server One Time Secret - Up and Running");
 });
 
 module.exports = router;
